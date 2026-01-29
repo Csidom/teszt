46a01a0b50684a3f384dfb960230a276f6182595
Csikszentmihályi Döme <csikszi.dome@gmail.com>
2026-01-29 13:46:31 +0100

    Harmadik osztály kész

---
+++ teszt/Harmadik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teszt
+{
+	internal class Harmadik
+	{
+		Dictionary<Adat, int> stat;
+
+		public Harmadik()
+		{
+			stat = new Dictionary<Adat, int>();
+		}
+	}
+}
